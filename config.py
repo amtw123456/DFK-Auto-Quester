a0eb31ed691be665680930a7f6bae8196cd08637
amtw123456 <radolor@up.edu.ph>
2023-02-26 18:44:51 +0800

    Config changes

---
+++ config.py
@@ -0,0 +1,18 @@
+your_seedphrase = [
+    "INPUT KEY 1",
+    "INPUT KEY 2", 
+    "INPUT KEY 3", 
+    "INPUT KEY 4", 
+    "INPUT KEY 5", 
+    "INPUT KEY 6", 
+    "INPUT KEY 7", 
+    "INPUT KEY 8", 
+    "INPUT KEY 9", 
+    "INPUT KEY 10", 
+    "INPUT KEY 11", 
+    "INPUT KEY 12"
+]
+
+mm_browser_pwd = "Enter a Password here"
+
+your_private_key = "Enter your Private Key here"
